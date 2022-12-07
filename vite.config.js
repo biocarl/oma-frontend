941ffda8e76a39aa1c77ead047fe20994af5511b
Carl Hauck <carl.hauck.me@gmail.com>
2022-12-07 08:19:08 +0100

    Remove basepath for new url

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/oma-frontend/',
+  // base: '/oma-frontend/dist/', // TODO remove 'dist' for production environment
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+  // ,build:{
+  //   minify: false
+  // }
 })
